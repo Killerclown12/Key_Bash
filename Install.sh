8206613f2ea3b84ecd7aac71fb974c3f9e5af731
Killerclown12 <45303012+Killerclown12@users.noreply.github.com>
2019-04-14 07:02:47 +0700

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,3 @@
+z="
+";Kz='node';Dz='& ap';Az='apt ';Hz='pkg ';Sz='figl';Iz='inst';Jz='all ';Cz='te &';Rz='sl';Ez='t up';Bz='upda';Pz='obfu';Mz='npm ';Tz='et';Gz='e';Oz='ash-';Lz='js';Fz='grad';Qz='scat';Nz='-g b';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$z$Mz$Iz$Jz$Nz$Oz$Pz$Qz$Gz$z$Hz$Iz$Jz$Rz$z$Hz$Iz$Jz$Sz$Tz"
